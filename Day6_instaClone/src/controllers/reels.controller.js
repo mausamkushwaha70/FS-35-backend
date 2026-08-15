21287635e210a2aabaecf8bcc9c2f1f0ea1ed94e
mausam <mausamkkushwaha@gmail.com>
2026-08-15 23:21:25 +0530

    feat: add reels-like functionality

--- Day6_instaClone/src/controllers/reels.controller.js
+++ Day6_instaClone/src/controllers/reels.controller.js
@@ -1,75 +1,120 @@
+import { json } from "express";
 import reelsModel from "../model/reels.model.js";
 import { sendFile } from "../services/storage.service.js";
 
 export const createReels = async (req, res) => {
-    try {
-        const { caption, location } = req.body || {};
-        console.log(req.body);
-        const file = req.file;
-        console.log(file);
-        if (!file) {
-            return res.status(400).json({
-                success: false,
-                message: "reels not found",
-            });
-        }
-
-        let media_Type;
-
-        if (file.mimetype.startsWith("video")) {
-             media_Type = "video";
-        }
-        else{
-            return res.status(400).json({
-                success: false,
-                message: "only video file can upload as reels",
-            });
-        }
-        //  upload to ImageKit
-        const uploadReels = await sendFile(file.buffer, file.originalname);
-        console.log(uploadReels)
-
-        // save reel in database {mongoDb}
-        const reels = await reelsModel.create({
-            user: req.user.id,
-            media_type: media_Type,
-            media_url: uploadReels.url,
-            media_fileID: uploadReels.fileId,
-            caption,
-            location,
-        });
-
-         return res.status(201).json({
-            success: true,
-            message: "Reel created successfully",
-            reels
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            success: false,
-            message: "Internal Server error",
-             error: error.message,
-        });
+  try {
+    const { caption, location } = req.body || {};
+    console.log(req.body);
+    const file = req.file;
+    console.log(file);
+    if (!file) {
+      return res.status(400).json({
+        success: false,
+        message: "reels not found",
+      });
     }
+
+    let media_Type;
+
+    if (file.mimetype.startsWith("video")) {
+      media_Type = "video";
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "only video file can upload as reels",
+      });
+    }
+    //  upload to ImageKit
+    const uploadReels = await sendFile(file.buffer, file.originalname);
+    console.log(uploadReels);
+
+    // save reel in database {mongoDb}
+    const reels = await reelsModel.create({
+      user: req.user.id,
+      media_type: media_Type,
+      media_url: uploadReels.url,
+      media_fileID: uploadReels.fileId,
+      caption,
+      location,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Reel created successfully",
+      reels,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server error",
+      error: error.message,
+    });
+  }
 };
 
-
-export const getReels = async (req,res)=>{
+export const getReels = async (req, res) => {
   try {
-   const allReels = await reelsModel.find()
-   return res.status(200).json({
-      success:true,
-      message:"All reels fetched",
-      allReels
-   })
+    const allReels = await reelsModel.find();
+    return res.status(200).json({
+      success: true,
+      message: "All reels fetched",
+      allReels,
+    });
   } catch (error) {
-   console.log(error);
-   return res.status(500).json({
-      success:false,
-      message:"Internal server error"
-   })
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
+};
 
-}
+export const likesReelsController = async (req, res) => {
+   try {
+      const reelId = req.params.id;
+      const reel = await reelsModel.findById(reelId);
 
+      if (!reel) {
+         return res.status(400).json({
+            success: false,
+            message: "reels not found",
+         });
+      }
+
+      if (String(reel.id) === req.user.id) {
+         return res.status(200).json({
+            success: true,
+            message: "you likes your own video",
+            reel,
+         });
+      }
+
+      const alreadyLike = reel.likes.includes(req.user.id);
+      if (alreadyLike) {
+         return res.status(200).json({
+            success: true,
+            message: "you already like your video",
+            reel,
+         });
+      }
+
+      reel.likes.push(req.user.id);
+      await reel.save();
+
+      return res.status(200).json({
+         success: true,
+         message: "reels likes successfully",
+         reel,
+         likes: reel.likes,
+         count: reel.likes.length,
+      });
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+         success: false,
+         message: "internal server error",
+      });
+   }
+};
--- Day6_instaClone/src/routes/reels.routes.js
+++ Day6_instaClone/src/routes/reels.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { createReels, getReels } from "../controllers/reels.controller.js";
+import { createReels, getReels, likesReelsController } from "../controllers/reels.controller.js";
 import { upload } from "../config/multer.js";
 const router = express.Router();
 
 
 router.post("/create-reels", authMiddleware, upload.single("video"), createReels)
 router.get("/get-reels",authMiddleware,getReels)
+router.post("/reel-likes/:id",authMiddleware, likesReelsController)
 
 export default router ;
