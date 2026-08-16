ab6617ed76a33cd509e96971ec3f110cbfda0831
mausam <mausamkkushwaha@gmail.com>
2026-08-16 20:17:47 +0530

    feat: Reel-veiws

--- Day6_instaClone/src/routes/reels.routes.js
+++ Day6_instaClone/src/routes/reels.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { createReels, getReels, likesReelsController } from "../controllers/reels.controller.js";
+import { createReels, getReels, likesReelsController, reelViewsController } from "../controllers/reels.controller.js";
 import { upload } from "../config/multer.js";
 const router = express.Router();
 
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post("/create-reels", authMiddleware, upload.single("video"), createReels)
 router.get("/get-reels",authMiddleware,getReels)
 router.post("/reel-likes/:id",authMiddleware, likesReelsController)
+router.post("/reel-views/:id",authMiddleware,reelViewsController)
+
 
 export default router ;
